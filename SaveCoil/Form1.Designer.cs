ac748a6cdba7ee5922d81f74f6182fafc6dca26d
yuan cheng <chengyuan82281681@hotmail.com>
2019-01-30 11:14:48 +1100

    An app that can store information with timestamp in a .csv file.

--- SaveCoil/Form1.Designer.cs
+++ SaveCoil/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SaveCoil
 {
-    partial class Form1
+    partial class SaveCoil
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.FileName = new System.Windows.Forms.Label();
+            this.Message = new System.Windows.Forms.Label();
+            this.Save = new System.Windows.Forms.Button();
+            this.Barcode = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // FileName
+            // 
+            this.FileName.AutoSize = true;
+            this.FileName.Location = new System.Drawing.Point(12, 27);
+            this.FileName.Name = "FileName";
+            this.FileName.Size = new System.Drawing.Size(51, 13);
+            this.FileName.TabIndex = 0;
+            this.FileName.Text = "FileName";
+            // 
+            // Message
+            // 
+            this.Message.AutoSize = true;
+            this.Message.Location = new System.Drawing.Point(13, 95);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(50, 13);
+            this.Message.TabIndex = 1;
+            this.Message.Text = "Message";
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(289, 43);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(112, 47);
+            this.Save.TabIndex = 2;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Barcode
+            // 
+            this.Barcode.Location = new System.Drawing.Point(12, 57);
+            this.Barcode.Name = "Barcode";
+            this.Barcode.Size = new System.Drawing.Size(271, 20);
+            this.Barcode.TabIndex = 3;
+            // 
+            // SaveCoil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(416, 128);
+            this.Controls.Add(this.Barcode);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.FileName);
+            this.Name = "SaveCoil";
+            this.Text = "SaveCoil";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label FileName;
+        private System.Windows.Forms.Label Message;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.TextBox Barcode;
     }
 }
 
--- SaveCoil/Form1.cs
+++ SaveCoil/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,26 @@ using System.Windows.Forms;
 
 namespace SaveCoil
 {
-    public partial class Form1 : Form
+    public partial class SaveCoil : Form
     {
-        public Form1()
+        public SaveCoil()
         {
             InitializeComponent();
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            string date = DateTime.Now.ToString("yyyy-MM-dd");
+            string path = @"C:\" + date + ".csv";
+            FileName.Text = path;
+            string coilID = Barcode.Text;
+            coilID = coilID.Replace('+', ',');
+            string dt = DateTime.Now.ToString("HH:mm:ss");
+            coilID = coilID + ", " + dt;
+            TextWriter txt = new StreamWriter(path, true); // true means text will be appended to the file.
+            txt.WriteLine(coilID);
+            txt.Close();
+            Message.Text = "CoilID " + coilID.Substring(0, 9) + " saved!";
         }
     }
 }
